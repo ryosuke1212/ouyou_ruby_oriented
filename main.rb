729cc16bee10d76b368b9c08221e0bdbd311b6f0
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-04-19 23:15:14 +0900

    fix: Getter, Setter, プロパティを使用しないように設定

--- main.rb
+++ main.rb
@@ -4,13 +4,12 @@ require './vending_machine'
 
 vm = VendingMachine.new
 
-drink = vm.buy(500, DrinkType::COKE)
+drink = vm.buy(Coin::FIVE_HUNDRED, DrinkType::COKE)
 charge = vm.refund
 
-if drink != nil && drink.kind == DrinkType::COKE then
+if drink != nil && drink.coke?
   print "コーラを購入しました。"
   print "お釣りは#{charge}です"
 else
   raise StandardError.new("コーラ買えんかった(´ﾟдﾟ｀)")
 end
-
