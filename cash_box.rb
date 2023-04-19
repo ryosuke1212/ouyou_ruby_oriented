65437bbdec4350ef48033a962a9def0146b93f03
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-04-20 01:16:57 +0900

    fix: クラスのインスタンスを2つまでにする

---
+++ cash_box.rb
@@ -0,0 +1,35 @@
+require './coin'
+
+class CashBox
+
+  def initialize(quantity)
+    @number_of_100yen = [Coin::ONE_HUNDRED] * quantity
+  end
+
+  def add(coin)
+    @number_of_100yen.push(coin)
+  end
+
+  def not_have_change?
+    size < 4
+  end
+
+  def take_out_change
+    coins = []
+    4.times do
+      coins.push(pop)
+    end
+    coins
+  end
+
+  private
+
+  def size
+    @number_of_100yen.length
+  end
+
+  def pop
+    @number_of_100yen.pop
+  end
+
+end
